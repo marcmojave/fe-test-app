5a03a4d9d4887b363ba7d966c80d57417e01097a
Marc Hickey <marc.hickey@gm.com>
2022-03-22 18:07:12 +0000

    Set up routing. Added styles.

---
+++ fe-test-app-master/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from "./users/users.component";
+import { CreateUserComponent } from "./users/create-user/create-user.component";
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'users/create',
+    component: CreateUserComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
