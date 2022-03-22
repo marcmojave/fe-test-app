5db9026717c2de7b8f72afc9a65467c88d57a68c
Marc Hickey <marc.hickey@gm.com>
2022-03-22 17:55:45 +0000

    Added custom validator for email. Added custom date pipe. Added file to hold ui messages.

---
+++ fe-test-app-master/src/app/shared/ui-text.ts
@@ -0,0 +1,7 @@
+export const userRetrievalErrorText = 'An error occurred when retrieving users. You may be able to fix this by' +
+  ' refreshing' +
+  ' the page.' +
+  ' \nOtherwise, please try again in a few minutes.'
+
+export const createUserErrorText = 'Failed to create user. You may be able to fix this by adding the user again' +
+  ' after a page refresh. Otherwise, please try again in a few minutes.'
--- fe-test-app-master/src/app/shared/validators.directive.ts
+++ fe-test-app-master/src/app/shared/validators.directive.ts
@@ -4,7 +4,7 @@ import {
   ValidationErrors,
   ValidatorFn
 } from "@angular/forms";
-import { UserService } from "../../services/user-service/user.service";
+import { UserService } from "../services/user.service";
 import { map } from "rxjs/operators";
 
 /**
@@ -36,4 +36,23 @@ export function duplicateName(userService: UserService): AsyncValidatorFn {
       })
     )
   };
+}
+
+/**
+ * This is a custom validator that can be used to validate entered emails are in the correct format.
+ * This can be used instead of the reactive Validators.email which considers name@domain to be valid.
+ *
+ * https://stackoverflow.com/questions/51767885/angular-form-email-validator-not-working-as-expected
+ * @param control
+ */
+
+// NOTE - I am aware of a discrepancy where the api considers an email of the format name@domain.i to be invalid.
+// I dont "think" it should be, so have not changed the validation to catch it here.
+export function emailValidator(control): { invalidEmail: boolean } | null {
+  if (control.value) {
+    const matches = control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/);
+    return matches ? null : { 'invalidEmail': true };
+  } else {
+    return null;
+  }
 }
